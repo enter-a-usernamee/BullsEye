ceb05a7137270237d14917268139efb4317e2a59
enter-a-usernamee <mjames29@live.spcollege.edu>
2023-08-20 14:59:34 -0400

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Malachi James on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
